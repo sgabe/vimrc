4bbb546a0bb8af12ab84c21f9c748583d47ac6ed
Amir Salihefendic <amix@doist.com>
2019-11-16 18:40:53 +0100

    Added gist-vim

--- update_plugins.py
+++ update_plugins.py
@@ -13,7 +13,6 @@ import requests
 
 from os import path
 
-
 #--- Globals ----------------------------------------------
 PLUGINS = """
 auto-pairs https://github.com/jiangmiao/auto-pairs
@@ -58,6 +57,7 @@ mru.vim https://github.com/vim-scripts/mru.vim
 rust.vim https://github.com/rust-lang/rust.vim
 vim-markdown https://github.com/plasticboy/vim-markdown
 comfortable-motion.vim https://github.com/yuttie/comfortable-motion.vim
+gist-vim https://github.com/mattn/gist-vim
 """.strip()
 
 GITHUB_ZIP = '%s/archive/master.zip'
@@ -75,8 +75,8 @@ def download_extract_replace(plugin_name, zip_path, temp_dir, source_dir):
     zip_f = zipfile.ZipFile(temp_zip_path)
     zip_f.extractall(temp_dir)
 
-    plugin_temp_path = path.join(temp_dir,
-                                 path.join(temp_dir, '%s-master' % plugin_name))
+    plugin_temp_path = path.join(
+        temp_dir, path.join(temp_dir, '%s-master' % plugin_name))
 
     # Remove the current plugin and replace it with the extracted
     plugin_dest_path = path.join(source_dir, plugin_name)
@@ -93,8 +93,7 @@ def download_extract_replace(plugin_name, zip_path, temp_dir, source_dir):
 def update(plugin):
     name, github_url = plugin.split(' ')
     zip_path = GITHUB_ZIP % github_url
-    download_extract_replace(name, zip_path,
-                             temp_directory, SOURCE_DIR)
+    download_extract_replace(name, zip_path, temp_directory, SOURCE_DIR)
 
 
 if __name__ == '__main__':
